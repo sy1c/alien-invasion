79c90bcc3f5ec3fa58e87cf68ae1b64d6cde8767
Mateusz Szymiec <mattszymiec@gmail.com>
2021-05-03 15:38:51 +0200

    Add spacecraft

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import sys
 import pygame
 
 from settings import Settings
+from objects import Spacecraft
 
 def run():
     pygame.init()
@@ -11,12 +12,15 @@ def run():
     screen = pygame.display.set_mode((settings.screen_width, settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
 
+    spacecraft = Spacecraft(screen)
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
 
         screen.fill(settings.bg_color)
+        spacecraft.blitme()
 
         pygame.display.flip()
 
